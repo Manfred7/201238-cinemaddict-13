d76bff02802e5cc0ecfe1c12cac221dff266a3b3
Manfred7 <manfred7@yandex.ru>
2020-11-19 23:28:44 +0300

    пропустил при коммите часть файлов

---
+++ src/main.js
@@ -0,0 +1,103 @@
+import {createFilmCardTemplate} from "./view/film-card.js";
+import {createFilmPopupTemplate} from "./view/film-popup.js";
+import {createShowMoreButtonTemplate} from "./view/show-more-button.js";
+import {createSiteMenuTemplate} from "./view/site-menu.js";
+import {createUsersTitleTemplate} from "./view/users-title.js";
+import {createSortTemplate} from "./view/sort.js";
+import {createFilmsTemplate} from "./view/films.js";
+import {createFooterStatisticsCardTemplate} from "./view/footer-statistics";
+import {createFilmsTopRatedTemplate} from "./view/films-top-rated";
+import {createFilmsMostCommentedTemplate} from "./view/films-most-commented";
+import {createStatsTemplate} from "./view/stats";
+import {createFilmsEmptyTemplate} from "./view/films-empty";
+
+const FILMS_COUNT = 5;
+const FILMS_EXTRA_COUNT = 2;
+
+const siteMainElement = document.querySelector(`.main`);
+
+const render = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const renderUserTitle = () => {
+  const header = document.querySelector(`.header`);
+  render(header, createUsersTitleTemplate(), `beforeend`);
+};
+
+const renderFooterStatistic = () =>{
+  const footerStatistics = document.querySelector(`.footer__statistics`);
+  render(footerStatistics, createFooterStatisticsCardTemplate(), `beforeend`);
+};
+
+const renderStats = () => {
+  renderUserTitle();
+
+  render(siteMainElement, createSiteMenuTemplate(), `beforeend`);
+  render(siteMainElement, createStatsTemplate(), `beforeend`);
+
+  renderFooterStatistic();
+};
+
+const renderListEmpty = () => {
+  renderUserTitle();
+  render(siteMainElement, createSiteMenuTemplate(), `beforeend`);
+  render(siteMainElement, createFilmsEmptyTemplate(), `beforeend`);
+
+  renderFooterStatistic();
+};
+
+const createFilms = () => {
+  const films = document.querySelector(`.films`);
+  const filmList = films.querySelector(`.films-list`);
+  const filmsContainer = filmList.querySelector(`.films-list__container`);
+
+  for (let i = 0; i < FILMS_COUNT; i++) {
+    render(filmsContainer, createFilmCardTemplate(), `beforeend`);
+  }
+
+  render(filmList, createShowMoreButtonTemplate(), `beforeend`);
+  render(films, createFilmsTopRatedTemplate(), `beforeend`);
+  render(films, createFilmsMostCommentedTemplate(), `beforeend`);
+
+  const filmExtraLists = document.querySelectorAll(`.films-list--extra`);
+
+  const filmsTopRatedContainer = filmExtraLists[0].querySelector(`.films-list__container`);
+  for (let i = 0; i < FILMS_EXTRA_COUNT; i++) {
+    render(filmsTopRatedContainer, createFilmCardTemplate(), `beforeend`);
+  }
+
+  const filmsMostCommentedContainer = filmExtraLists[1].querySelector(`.films-list__container`);
+  for (let i = 0; i < FILMS_EXTRA_COUNT; i++) {
+    render(filmsMostCommentedContainer, createFilmCardTemplate(), `beforeend`);
+  }
+};
+
+const renderList = () => {
+  renderUserTitle();
+  render(siteMainElement, createSiteMenuTemplate(), `beforeend`);
+  render(siteMainElement, createSortTemplate(), `beforeend`);
+  render(siteMainElement, createFilmsTemplate(), `beforeend`);
+  createFilms();
+  renderFooterStatistic();
+};
+
+const renderPopup = () => {
+  render(siteMainElement, createSiteMenuTemplate(), `beforeend`);
+  render(siteMainElement, createSortTemplate(), `beforeend`);
+  render(siteMainElement, createFilmsTemplate(), `beforeend`);
+  createFilms();
+  renderFooterStatistic();
+  render(siteMainElement, createFilmPopupTemplate(), `beforeend`);
+};
+
+window.main = {
+  doRenderPopup: renderPopup, // отрисовка элементов как в popup.html
+  doRenderListEmpty: renderListEmpty, // отрисовка как в list-empty.html
+  doRenderList: renderList, // отрисовка основных элементов, как в list.html
+  doRenderStats: renderStats // отрисовка статистики как в stats.html
+};
+
+window.main.doRenderPopup();
+
+
